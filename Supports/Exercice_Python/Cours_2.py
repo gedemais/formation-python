a010ae9d9bd531d83bd193d04e5bba8f6027f0c4
Nicolas Jouneau <njouneau@gmail.com>
2022-04-19 17:30:44 +0200

    MODIFIED Cours2.py

--- Supports/Exercice_Python/Cours_2.py
+++ Supports/Exercice_Python/Cours_2.py
@@ -1,10 +1,13 @@
 """
 Cours 2
-"""
+
 import math
+from multiprocessing.connection import Listener
 from operator import length_hint;
 
-
+"""
+1
+"""
 a = float(input("Entrer un float : "))
 racine = 0
 if a >= 0.0 :
@@ -12,7 +15,9 @@ if a >= 0.0 :
     print(racine)
 else : print("Le chiffre est négatif")
 
-
+"""
+2
+"""
 b = input("Entrer un mot : ")
 c = input("Entrer un mot : ")
 len_b = len(b)
@@ -22,7 +27,9 @@ if len_b > len_c:
 else :
     print("Le deuxieme mot est plus long")
 
-
+"""
+3
+"""
 def check_status(Pression:float, Volume:float):
     pSeuil = 2.3
     vSeuil = 7.41
@@ -36,7 +43,9 @@ def check_status(Pression:float, Volume:float):
         print("Tout va bien")
 check_status(float(input("Pression ? ")), float(input("Volume ? ")))
 
-
+"""
+4
+"""
 A = 0
 B = 10
 while A<B : 
@@ -47,3 +56,26 @@ while B != 0:
         print(B)
     B = B - 1
 
+"""
+5
+"""
+
+n = int(input("Entrez un nombre entre 1 et 10 : "))
+while (n < 1 or n > 10) :
+    n = int(input("Essaye encore :"))
+print(n)
+
+
+6
+"""
+
+Phrase = input("Entrer une phrase : ")
+Liste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for n in Phrase :
+    if n == "\o":
+        break
+    print(n)
+for n in Liste :
+    if n == "\o":
+        break
+    print(n)
