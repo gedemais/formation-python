cae8907abcf2becb73ae02a2dd670dafcd3f48fb
Fatou <doucoure-mf@hotmail.fr>
2022-04-19 16:13:27 +0200

    init commit

--- exercises/script.py
+++ exercises/script.py
@@ -2,13 +2,14 @@
 temps=6.892
 distance=19.7
 vitesse=temps/distance
-'vitesse'[1]
-print('vitesse={}'.format(vitesse))
+print('vitesse={:01.1f}'.format(vitesse))
 #Exercice 1 question 2
-nom=input()
+nom=input('entrer le nom')
 print('nom={}'.format(nom))
-age=input()
+age=input('entrer age')
 print('age={}'.format(nom))
-nom=raw_input()
-print('nom{}'.format(nom))
+nom=raw_input('entrer le nom')
+print('nom={}'.format(nom))
+#Exercice 2
+#1 flottant
 
