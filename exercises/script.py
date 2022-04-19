a86479a66989174a9b6ec4f929a554e0c98b3d6e
Fatou <doucoure-mf@hotmail.fr>
2022-04-19 15:38:55 +0200

    init commit

---
+++ exercises/script.py
@@ -0,0 +1,14 @@
+# Exercice 1 question 1
+temps=6.892
+distance=19.7
+vitesse=temps/distance
+'vitesse'[1]
+print('vitesse={}'.format(vitesse))
+#Exercice 1 question 2
+nom=input()
+print('nom={}'.format(nom))
+age=input()
+print('age={}'.format(nom))
+nom=raw_input()
+print('nom{}'.format(nom))
+
