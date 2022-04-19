148f0f61d7dc7db00c14710daaa32fcd1e16c42a
Audrey <you@gmail.com>
2022-04-19 15:50:39 +0200

    init commit

--- exercises/script.py
+++ exercises/script.py
@@ -2,9 +2,7 @@ temps = 6.892
 distance = 19.7
 vitesse = distance / temps
 print ('vitesse{}' .format(vitesse))
-git add -A .
-git commit -m "init commit"
-git push --set-upstream origin<audreynaliali>
+
 
 
 
