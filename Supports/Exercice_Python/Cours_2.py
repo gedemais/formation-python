175f4481dfb4bc5dabf6230a2dd253ec97141cf5
Nicolas Jouneau <njouneau@gmail.com>
2022-04-19 15:55:38 +0200

    ADD question 2

--- Supports/Exercice_Python/Cours_2.py
+++ Supports/Exercice_Python/Cours_2.py
@@ -1,11 +1,21 @@
 """
 Cours 2
 """
-import math;
+import math
+from operator import length_hint;
 
 a = float(input("Entrer un float : "))
 racine = 0
 if a >= 0.0 :
-    racine = math.sqrt(a)
+    racine = round(math.sqrt(a), 2)
     print(racine)
 else : print("Le chiffre est négatif")
+
+b = input("Entrer un mot : ")
+c = input("Entrer un mot : ")
+len_b = len(b)
+len_c = len(c)
+if len_b > len_c:
+    print("Le premier mot est plus long")
+else :
+    print("Le deuxieme mot est plus long")
