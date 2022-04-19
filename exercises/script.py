8fbff5750884fc107c9f9ce3fda88fe97ed8e55f
PaulineB <pao.linh380@gmail.com>
2022-04-19 15:38:55 +0200

    init commit

---
+++ exercises/script.py
@@ -0,0 +1,9 @@
+# affecter les variables
+temps = 6.892
+distance = 19.7
+# définir la vitesse 
+vitesse = distance/temps
+# afficher résultat de la vitesse
+print(vitesse)
+#afficher un chiffre après la virgule
+print('vitesse={}'.format(vitesse))
