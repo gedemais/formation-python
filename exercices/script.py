d7f8c6e1714a193651c91bf63101d1930db18907
francine <francine.sonia@yahoo.fr>
2022-04-20 10:06:29 +0200

    init commit

--- exercices/script.py
+++ exercices/script.py
@@ -2,6 +2,20 @@ temps = 6.892
 distance = 19.7
 vitesse = distance / temps
 print ('vitesse={}'.format(vitesse)) 
+Roundvitesse = round(vitesse , 1)
+print (Roundvitesse)
+
+#2
+#f=3.123
+from math import sqrt
+f = float (input('entrez un nombre flottant'))
+if f >= 0 :
+    print (sqrt (f))
+else: 
+    print ("message d'erreur")
+
+
+
 
 
 
