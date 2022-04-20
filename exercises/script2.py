823900f9888efddf554e340f145fbbb2d17b1d81
Sarah <sarahjustine.akakpo@gmail.com>
2022-04-20 10:58:30 +0200

    exercice sarah a

--- exercises/script2.py
+++ exercises/script2.py
@@ -1,7 +1,13 @@
 #QUEST3
 Pseuil = 2.3
-Vseuil = 7,41
+Vseuil = 7.41
 Pression = float(input("valeur pression"))
 Volume = float(input("valeur volume"))
 if (Volume > Vseuil) and (Pression > Pseuil) :
     print ("arret immédiat")
+if Pression > Pseuil :
+    print ("augmentez le volume de l'enceinte")
+if Volume > Vseuil :
+    print ("diminuez le volume")
+if (Volume < Vseuil) and (Pression < Pseuil) :
+    print ("tout va bien")
