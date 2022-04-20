c40f147c2708c6827c888803a83acdcf2120c610
Sarah <sarahjustine.akakpo@gmail.com>
2022-04-20 10:03:01 +0200

    exo deux sarahakp

--- exercises/script.py
+++ exercises/script.py
@@ -1,13 +1,15 @@
-temps = 6.982
-distance = 19.7
-vitesse = distance / temps
-print('vitesse={}'.format(vitesse))
+from math import sqrt
+f = float (input("entrez un nombre flottant"))
+if f>=0 : 
+    print (sqrt(f))
+else : 
+    print ("erreur")
 
-
-
-
-
-
-
-
-
+#QUEST2
+print ("entrez deux mots")
+a = input("un premier mot")
+b = input("un deuxieme mot")
+if a>b :
+    print ("b est le plus petit")
+else : 
+    print ("b est le plus grand")
