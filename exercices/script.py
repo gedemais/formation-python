9ab28faf7ff5c36e59cf5a3bf7559b1ebc929b40
Suzana <lemur_m@yahoo.com>
2022-04-20 08:44:46 +0200

    init commit

--- exercices/script.py
+++ exercices/script.py
@@ -2,3 +2,12 @@ temps = 6.892
 distance = 19.7
 vitesse = distance/temps
 print('vitesse={}'.format(vitesse))
+
+#2
+
+import sqrt from math
+a = float(input('saisir un nombre flottant'))
+if a >= 0 :
+    print (sqrt (a))
+else: 
+    print ("erreur")
