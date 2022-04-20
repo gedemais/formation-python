e05a2f36e64e7c7b370f55ba702da15ce4a4eb56
Sara <aitelhajsara@gmail.com>
2022-04-20 10:06:26 +0200

    init commit

--- exercices/script.py
+++ exercices/script.py
@@ -1,4 +1,16 @@
 temps= 6.892
 distance= 19.7
 vitesse=distance / temps
-print('vitesse={}' .formation(vitesse))
+print(vitesse)
+
+
+
+#ex 2
+#f=10.555
+from math import sqrt
+f = float (input('entrez un nombre flottant'))
+if f >= 0 :
+    print (sqrt (f))
+else:
+    print("message d'erreur")
+    
