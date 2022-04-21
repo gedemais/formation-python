07b472651d21d44cb0bac7ea990072c85b891d33
francine <francine.sonia@yahoo.fr>
2022-04-21 11:26:47 +0200

    init commit

--- exercices/fonction1.py
+++ exercices/fonction1.py
@@ -1,6 +1,15 @@
 def cube(x):
     """Calcule le cube de l'argument."""
     return x**3
-def volumeSphere(r):
-    """Calcule le volume d'une sphere de rayon <r>."""
-    return 4 * pi * cube(r) / 3
+x = int (input ("saisir un nombre"))
+print("le cube de", x , "est" , cube(x))
+
+from math import pi 
+
+def volumesphere(r) :
+    v = 4 / 3 * pi * cube(r)
+    """formule de la fonction qui permet de calculer le volume d'une sphère"""
+    return (v)
+r = int(input("saisir un nombre : "))
+print("le volume de la sphere ", volumesphere(r)) 
+
