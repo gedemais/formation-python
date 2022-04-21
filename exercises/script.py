bf97031a47f1722d870e0a2c9c2bd7b1e9a218ca
Audrey <you@gmail.com>
2022-04-21 11:28:25 +0200

    init commit

--- exercises/script.py
+++ exercises/script.py
@@ -13,3 +13,27 @@ from math import sqrt
 
 
 
+#3) les fonctions
+#1) 
+#def table(base, debut, fin, inc) :
+#  ""affiche la table des <base>, de <debut> a <fin>, de <inc>.""
+ # n = debut
+  # while n <= fin :
+   #    print(n, 'x', base, '=', n*base)
+    #   n = n + inc
+#table (2, 1, 10, 2)
+
+#2)
+from math import pi
+def cube (x) :
+    """calcule le cube d'un nombre."""
+    return x**3
+x = int(input("Veuillez saisir un nombre: "))
+print ("le cube de" , x , "est :" ,cube(x))
+
+from math import pi
+def Volumesphere(r) :
+    v = 4 / 3 * pi * cube(r)
+    return (v)
+r = int(input("veuillez saisir un nombre :"))
+print("le volume de la sphere est" ,Volumesphere(r))  
