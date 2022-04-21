9d0cf04a16eee96c9c3e8e7e4794482218093e35
francine <francine.sonia@yahoo.fr>
2022-04-21 09:40:53 +0200

    init commit

---
+++ exercices/fonction1.py
@@ -0,0 +1,6 @@
+def cube(x):
+    """Calcule le cube de l'argument."""
+    return x**3
+def volumeSphere(r):
+    """Calcule le volume d'une sphere de rayon <r>."""
+    return 4 * pi * cube(r) / 3
