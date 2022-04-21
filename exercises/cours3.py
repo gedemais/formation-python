a0abdf701de09098aa91d9efae1a9e89e88fc0bb
Fatou <doucoure-mf@hotmail.fr>
2022-04-21 11:13:22 +0200

    exo 4

--- exercises/cours3.py
+++ exercises/cours3.py
@@ -1,11 +1,11 @@
 #1
 def table(base, debut, fin, inc):
-    x = debut
-    y = fin
-    while x < fin:
-        print(x,y)
-        x = x + inc
-table(1, 2, 3, 4)
+    n=debut
+    while n <= fin :
+        print(n, 'x', base, '=', n * base)
+        n = n + inc
+print('la table est')
+table(1,1,9,2)
 
 #2
 from math import pi
@@ -22,10 +22,27 @@ print(volumeSphere(r))
 #3
 def cube(x):
     return x**3
-def mafonction(x):
+def f(x):
     return 2 * cube(x) + x - 5
-def tabuler(fonction, borneinf, bornesup, nbpas):
-    while borneinf < bornesup:
-    print(mafonction(x))
+def tabuler (f, borneinf, bornesup, nbpas):
+    for x in range(borneinf, bornesup, nbpas):
+        print('la valeur de x est',x)
+        print('f(x)=',f(x))
+borneinf=int(input('quelle est la borne inf?'))
+bornesup=int(input('quelle est la borne sup?'))
+nbpas=int(input('quelle est le nb de pas?'))
+while nbpas <= 0 or borneinf >= bornesup:
+    print('error')
+    borneinf=int(input('quelle est la borne inf?'))
+    bornesup=int(input('quelle est la borne sup?'))
+    nbpas=int(input('quelle est le nb de pas?'))
+tabuler(f, borneinf, bornesup, nbpas)
 
-
+#4
+from math import pi
+def  volumeEllipsoide(a, b, c):
+    return (4/3) * pi * a * b * c
+def  masseEllipsoide(a,b,c):
+    return volume * rho
+print('le volume est', volumeEllipsoide, 'et la masse est', masseEllipsoide)
+volumeEllipsoide(1, 1, 1)
